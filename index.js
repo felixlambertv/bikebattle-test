e8a0bf3d256b9bc34e1df38d7a5f91fa2dc0177d
Felix Lambert <felix@itsavirus.com>
2023-01-31 12:09:56 +0800

    Updating test script, remove unused

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ async function main() {
   const [metamask, page] = await dappeteer.bootstrap(puppeteer, {
     metamaskVersion: "v10.15.0",
     headless: false,
+    args: [
+      "--start-maximized"
+    ],
     defaultViewport: null,
   });
 
@@ -28,48 +31,30 @@ async function main() {
   await page.bringToFront();
   await sleep(2000);
 
-  const selector = await page.$x('//*[@id="home"]/header/div/div/div[2]/button');
-  await selector[0].click();
+  const metamaskBtn = await page.$x('//*[@id="home"]/header/div/div/div[2]/button');
+  await metamaskBtn[0].click();
   await metamask.approve();
   await metamask.sign();
   await page.bringToFront();
-  await sleep(10000);
+  await sleep(6000);
   const raceClick = await page.$x('//*[@id="worldMap"]/div[2]/div[3]/a/button');
   await raceClick[0].click();
-  const trackClick = await page.$x('//*[@id="root"]/div/div[2]/div[1]/div');
+  await page.waitForNavigation({
+    waitUntil: "networkidle0"
+  });
+  const trackClick = await page.$x('//*[@id="root"]/div/div[2]/div[1]/div/div/div');
   await trackClick[0].click();
-
-  //   var originSelector = await page.$x(
-  //     '//button[contains(@class,"MuiButtonBase-root")]'
-  //   );
-
-  //   await originSelector[0].click();
-  //   await originSelector[0].click();
-  //   var input = await page.waitForSelector("input#token-search-input");
-  //   await input.type("BNB");
-  //   input = await page.$x(
-  //     '//div[contains(@class,"token-item-0x264c1383EA520f73dd837F915ef3a732e204a493")]'
-  //   );
-  //   await input[0].click();
-  //   await sleep(2000);
-  //   await originSelector[1].click();
-  //   var input = await page.waitForSelector("input#token-search-input");
-  //   await input.type("USDC");
-  //   input = await page.$x('//div[contains(@class,"token-item-")]');
-  //   await input[0].click();
-  //   await sleep(2000);
-  //   input = await page.$x('//*[contains(@title,"Token Amount")]');
-  //   await input[0].type("1");
-  //   await sleep(1000);
-  //   input = await page.$x('//button[contains(text(),"Connect Wallet")]');
-  //   await input[0].click();
-  //   input = await page.waitForSelector("button#connect-METAMASK");
-  //   await input.click();
-  //   await metamask.approve({ allAccounts: false });
-  //   page.bringToFront();
-  //   input = await page.waitForSelector("button#swap-button");
-  //   await input.click();
-  //   await metamask.confirmTransaction();
+  const bikeSelector = await page.$x('//*[@id="root"]/div/div[3]/div[2]/div[2]/div/div/div');
+  await bikeSelector[0].click();
+  const joinRaceBtn = await page.$x('//*[@id="root"]/div/div[3]/div[3]/div[1]');
+  await joinRaceBtn[0].click();
+  await sleep(5000);
+  await page.waitForSelector('.MuiBox-root', { visible: true });
+  const startRaceBtn = await page.$x('/html/body/div[2]/div[3]/div[2]/div/div[4]/div');
+  await startRaceBtn[0].click();
+  await sleep(10000);
+  await page.close();
+  process.exit(0);
 }
 
 main();
